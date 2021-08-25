6f162027cb892f8e965e8cf7f697e41cae35ed20
JimmyMcBride <mcbride967@gmail.com>
2021-08-25 08:34:20 -0500

    touch ups

--- src/pages/Topic.vue
+++ src/pages/Topic.vue
@@ -11,44 +11,83 @@
         <q-breadcrumbs-el :label="topic.title" />
       </q-breadcrumbs>
     </q-toolbar>
-    <!-- <div class="text-h3 q-ml-xl">{{ topic.title }}</div> -->
     <div class="q-px-xl q-py-md">
-      <!-- <div class="q-pa-md"> -->
+      <!-- <q-card class="q-pa-md q-ma-md">
+        <topic-item
+          v-for="discussion in discussions"
+          :discussion="discussion"
+          :key="discussion.published.seconds"
+        >
+        </topic-item>
+        <q-card class="flex justify-center q-mt-md">
+          <q-pagination v-model="page" :max="5" input />
+        </q-card>
+      </q-card> -->
       <q-table
         :title="topic.title"
         :data="discussions"
         :columns="columns"
         row-key="title"
         :pagination="pagination"
-      />
-      <!-- </div> -->
+        :loading="topicLoading"
+        :dense="$q.screen.lt.md"
+        :grid="$q.screen.lt.sm"
+        style="text-align: center"
+      >
+        <template v-slot:body-cell-pinned="props">
+          <q-td :props="props">
+            <q-icon v-show="props.value" name="fas fa-thumbtack" size="1.5em" />
+          </q-td>
+        </template>
+        <template v-slot:body-cell-published="props">
+          <q-td :props="props">
+            <!-- <q-icon v-show="props.value" name="fas fa-thumbtack" size="1.5em" /> -->
+            <div>{{ format_date(props.value) }}</div>
+          </q-td>
+        </template>
+      </q-table>
     </div>
   </div>
 </template>
 
 <script>
+// import TopicItem from "components/TopicItem.vue"
+import moment from "moment"
 import { mapActions } from "vuex"
 
 export default {
+  // components: { TopicItem },
   data() {
     return {
       columns: [
         { name: "title", label: "Title", field: "title", align: "left" },
-        { name: "likes", label: "Likes", field: "likes", align: "left" },
+        {
+          name: "username",
+          label: "Username",
+          field: "username",
+          align: "center",
+        },
+        { name: "likes", label: "Likes", field: "likes", align: "center" },
         {
           name: "dislikes",
           label: "Dislikes",
           field: "dislikes",
-          align: "left",
+          align: "center",
         },
         {
           name: "comments",
           label: "comments",
           field: "totalComments",
-          align: "left",
+          align: "center",
         },
-        { name: "views", label: "Views", field: "views", align: "left" },
-        // { name: "pinned", label: "Pinned", field: "pinned" },
+        { name: "views", label: "Views", field: "views", align: "center" },
+        {
+          name: "published",
+          label: "Published",
+          field: "published",
+          align: "center",
+        },
+        { name: "pinned", label: "Pinned", field: "pinned", align: "center" },
       ],
       pagination: {
         // sortBy: 'desc',
@@ -61,6 +100,10 @@ export default {
   },
   methods: {
     ...mapActions("store", ["loadTopic", "loadDiscussions"]),
+    format_date(date) {
+      console.log(`CURR DATE: ${new Date(date.seconds)}`)
+      return moment(new Date(date.seconds * 1000)).format("MMM D, YYYY")
+    },
   },
   mounted() {
     this.loadTopic(this.$route.params.topicSlug)
@@ -69,6 +112,9 @@ export default {
   computed: {
     topic() {
       return this.$store.getters["store/topic"]
+    },
+    topicLoading() {
+      return this.$store.getters["store/topicLoading"]
     },
     discussions() {
       console.log("discussions:", this.$store.getters["store/discussions"])
